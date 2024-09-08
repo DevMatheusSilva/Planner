00894eb9534c1687ebec6f174fdf5d2ed85ad29f
= DevMatheusSilva <= matheusmello225@gmail.com>
2024-09-08 17:42:20 -0300

    refactor: created a form base layout for both login and register pages

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,20 +1,28 @@
 import { Request, Response } from "express";
 
+const BASE_LAYOUT = "./layouts/formLayout";
+const LAYOUT_STYLE_LINK = "../styles/form.css";
+
+interface RenderInfo {
+    title: string;
+    cssLink: string;
+    page: string;
+    type: string;
+}
+
 export default class UserController {
-    constructor() {
+    constructor() {}
+
+    private renderPage(res: Response, title: string, page: string, type: string, status: number = 200) {
+        const objToRender: RenderInfo = { title, cssLink: LAYOUT_STYLE_LINK, page, type };
+        res.status(status).render(BASE_LAYOUT, objToRender);
     }
 
-    static renderLoginPage(_: Request, res: Response) {
-        res.render(
-            "./pages/login",
-            { title: "Planner - Welcome !!", cssLink: "../styles/login/login.css" }
-        );
+    public renderLoginPage(_: Request, res: Response) {
+        this.renderPage(res, "Planner - Welcome !!", "../pages/login", "login");
     }
 
-    static renderRegisterPage(_: Request, res: Response) {
-        res.render(
-            "./pages/register",
-            { title: "Planner - Join Us !!", cssLink: "../styles/register/register.css" }
-        );
+    public renderRegisterPage(_: Request, res: Response) {
+        this.renderPage(res, "Planner - Join Us !!", "../pages/register", "register");
     }
 }
--- src/controllers/routes/userRouter.ts
+++ src/controllers/routes/userRouter.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import UserController from "../UserController";
 
+const userController = new UserController();
 const userRouter = Router();
 
-userRouter.get('/login', UserController.renderLoginPage);
-userRouter.get('/register', UserController.renderRegisterPage);
+userRouter.get('/login', (req: Request, res: Response) => userController.renderLoginPage(req, res));
+userRouter.get('/register', (req: Request, res: Response) => userController.renderRegisterPage(req, res));
 
 export default userRouter;
