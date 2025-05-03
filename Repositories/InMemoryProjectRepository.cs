3c204b060c02dde953fa3178b563c1e7565cf3c5
DevMatheusSilva <matheusmello225@gmail.com>
2025-05-03 14:24:00 -0300

    feat: created in-memory implementation for project repository

---
+++ Repositories/InMemoryProjectRepository.cs
@@ -0,0 +1,29 @@
+﻿using PlannerApp.Entities;
+using PlannerApp.Interfaces;
+
+namespace PlannerApp.Repositories;
+
+public class InMemoryProjectRepository : IProjectRepository
+{
+    private readonly List<Project> _projects = [];
+    public void Save(Project project)
+    {
+        project.Id = GenerateNextId();
+        _projects.Add(project);
+    }
+
+    public List<Project> GetAll()
+    {
+        return _projects;
+    }
+
+    public Project? GetById(int id)
+    {
+        return _projects.FirstOrDefault(x => x.Id == id);
+    }
+    
+    private int GenerateNextId()
+    {
+        return _projects.Count + 1;
+    }
+}
